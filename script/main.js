1e10ee6fa11bfd3f669b470d4361aaac6a3121b4
crisdenoronha <crisdenoronha@gmail.com>
2022-02-23 18:58:49 -0500

    week4

--- script/main.js
+++ script/main.js
@@ -1,46 +1,43 @@
 (() => {
 	// collect the bottons at the bottom of the page
-	let theThumbnails = document.querySelectorAll("#buttonHolder img");
-	    gameBoard = document.querySelector(".puzzle-board"),
-			puzzlePieces = document.querySelectorAll(".puzzle-pieces img"),
-			dropZones = document.querySelectorAll(".drop-zone"),
-			testLink = document.querySelector("a");
+	let theThumbnails = document.querySelectorAll("#buttonHolder img"),
+			gameBoard = document.querySelector(".puzzle-board"),
+			puzzlePieces = document.querySelectorAll(".puzzle-pieces *"),
+			dropZones = document.querySelectorAll(".drop-zone");
 
 	function changeBgImg() {
 		//debugger;
 		gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgref}.jpg)`;
 	}
 
-	function dragStrarted() {
+	function dragStrarted(event) {
 		console.log('started draggin a piece');
-		evenr.dataTranfer.setData('currentItem', event.target.id);
-  }
+		//use the setData method of the drag event to store a reference to the current element
+		event.dataTransfer.setData('currentItem', event.target.id);
+	}
 
-	function allowDragOver() {
+	function allowDragOver(event) {
 		event.preventDefault();
 		console.log('dragged over me');
 	}
 
 	function allowDrop(event) {
 		event.preventDefault();
-		console.log('dropped on me');
+		console.log('drop on me');
+		//retrieve the dragged element using the dataTransfer object
+		//this was set in the drag event using the setData method
+		let droppedEl = event.dataTransfer.getData('currentItem');
+		console.log(droppedEl);
 
-	let droppedEl = event.dataTranfer.getData('currentItem');
-	this.appendChild(document.querySelector(`#${droppedEl}`));
-	
-	console.log(droppedEl);
+		this.appendChild(document.querySelector(`#${droppedEl}`));
 	}
+		//add event handling here-> loop through theThumbnails array and add event handing to each image
+	theThumbnails.forEach(item => item.addEventListener("click", changeBgImg));
 
-	function turnAnchorOff(e) {
-		e.preventDefault();
-	}
-	//add event handling here-> loop through theThumbnails array and add event handing to each image
- theThumbnails.forEach(thumb => thumb.addEventListener("click", changeBgImg));
+	puzzlePieces.forEach(piece => piece.addEventListener("dragstrart", dragStrarted));
 
- puzzlePieces.forEach(piece => piece.addEventListener("dragstrart", dragStrarted));
-
- dropZones.forEach(zone => zone.addEventListener("dragover", allowDragOver));
-  zone.addEventListener("dragover", allowDragOver);
-	zone.addEventListener("drop", allowDrop);
-
+	dropZones.forEach(zone => {
+		zone.addEventListener("dragover", allowDragOver);
+		zone.addEventListener("drop", allowDrop);
+	});
 })();
